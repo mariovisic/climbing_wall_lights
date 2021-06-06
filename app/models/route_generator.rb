fa02494bf6d35acbb30a51f352b7478d9f5471bd
Mario Visic <mario@mariovisic.com>
2021-06-06 16:14:00 +1000

    Add a hand class to the route generator
    This class will hold onto a position and know if it's the left or right
    hand, this will be useful when we start to detect when hands are crossed
    over :)

--- app/models/route_generator.rb
+++ app/models/route_generator.rb
@@ -1,10 +1,14 @@
+require 'forwardable'
+
 class RouteGenerator
   STARTING_ROWS = 4..6
   HAND_SPLIT_RANGE = 1..4
 
   def initialize
     @state = { }
-    @hands = { }
+    @left_hand = Hand.new(:left)
+    @right_hand = Hand.new(:right)
+    @hands = [@left_hand, @right_hand]
   end
 
   def self.generate
@@ -24,31 +28,30 @@ class RouteGenerator
   private
 
   def move_hand
-    @hands[:left].lower_than?(@hands[:right]) ?  :left : :right
+    @hands.min { |hand_a, hand_b| hand_a.x_position <=> hand_a.x_position }
   end
 
   def other_hand
-    @hands[:right].lower_than?(@hands[:left]) ?  :left : :right
+    @hands.max { |hand_a, hand_b| hand_a.x_position <=> hand_a.x_position }
   end
 
   def set_start_holds
     starting_positions = available_positions(STARTING_ROWS.begin, STARTING_ROWS.end)
 
-    @hands[:left] = set(starting_positions.sample, 'start')
-    @hands[:right] = set((starting_positions.select { |position| position.in_range?(@hands[:left]) }).sample, 'start')
+    set(@left_hand, starting_positions.sample, 'start')
+    set(@right_hand, (starting_positions.select { |position| position.in_range?(@left_hand) }).sample, 'start')
   end
 
-  def set(position, state=nil)
+  def set(hand, position, state=nil)
     if state.nil?
       state = position.near_top? ? 'finish' : 'on'
     end
     @state[position.to_s] = state
-
-    position
+    hand.position = position
   end
 
   def hands_are_near_the_top
-    @hands.values.any?(&:near_top?)
+    @hands.any?(&:near_top?)
   end
 
   def available_positions(y_start, y_end = Wall::VERTICAL-1)
@@ -62,11 +65,33 @@ class RouteGenerator
   end
 
   def set_hand_hold
-    positions = available_positions(@hands[move_hand].y).select do |position|
-      position.in_range?(@hands[other_hand])
+    positions = available_positions(move_hand.y).select do |position|
+      position.in_range?(other_hand)
     end
 
-    @hands[move_hand] = set(positions.sample)
+    set(move_hand, positions.sample)
+  end
+
+  class Hand
+    extend Forwardable
+
+    def_delegators :@position, :x, :y, :near_top?
+
+    attr_reader :side
+
+    def initialize(side)
+      @side = side
+    end
+
+    def position=(position)
+      @position = position
+      @x_rand = rand
+    end
+
+    # If left and right hands are equal then randomly select which to move :)
+    def x_position
+      @position.x + @x_rand
+    end
   end
 
   class Position
@@ -77,19 +102,14 @@ class RouteGenerator
       @y = y
     end
 
-    def near_top?
-      y >= Wall::VERTICAL - 2
-    end
-
-    def lower_than?(position)
-      # If left and right hands are equal then randomly select which to move :)
-      y < position.y || (y == position.y && rand(2) == 0)
-    end
-
     def in_range?(other_hand)
       distance_to_other_hand = ((((x - other_hand.x).abs ** 2) + ((y - other_hand.y).abs ** 2)) ** 0.5)
 
       self != other_hand && HAND_SPLIT_RANGE.include?(distance_to_other_hand)
+    end
+
+    def near_top?
+      y >= Wall::VERTICAL - 2
     end
 
     def to_s
